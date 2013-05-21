760b7f0a430a288db7c554d893d22a91fea56a45
YuYang <edyuy@zmyseries.com>
2013-05-21 21:30:35 +0800

    more generic

--- iframe_upload.php
+++ iframe_upload.php
@@ -1,53 +1,31 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <link href="../assets/css/bootstrap.css" rel="stylesheet">
-    <link href="../assets/css/bootstrap-responsive.css" rel="stylesheet">
-    <script src="../assets/js/jquery.js"></script>
-    <script src="../assets/js/bootstrap.js"></script>
-    <title></title>
+    <title>iFrame_upload</title>
 </head>
 <body>
-<div class="container">
     <iframe name="iframeUpload" style="display:none"></iframe>
     <script type="text/javascript">
-        function toggleModal(title, body) {
-            document.getElementById('modalLabel').innerHTML = title;
-            document.getElementById('modalBody').innerHTML = body;
-            $('#modal').modal('toggle');
-        }
         function uploadStart() {
-            $("#submit").button("loading");
+            var submitBtn = document.GetElementById('submit');
+			submitBtn.className = "disabled";
+			submitBtn.innerText = "Uploading...";
         }
         function uploadOver() {
-            $("#submit").button("reset");
+            var submitBtn = document.GetElementById('submit');
+			submitBtn.className = "";
+			submitBtn.innerText = "Upload";
         }
+		function toggleModal(str) {
+			alert(str);
+		}
     </script>
-    <div id="modal" class="modal hide fade" tabindex="-1" role="dialog" aria-labelledby="modalLabel" aria-hidden="true">
-        <div class="modal-header">
-            <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
-            <h3 id="modalLabel">信息</h3>
-        </div>
-        <div class="modal-body">
-            <p id="modalBody"></p>
-        </div>
-        <div class="modal-footer" id="modal-footer-div">
-            <button class="btn" data-dismiss="modal" aria-hidden="true" id="modal-footer">关闭</button>
-        </div>
-    </div>
-    <form class="form-horizontal" name="form" method="post" action="generic_upload_server.php" enctype="multipart/form-data" target="iframeUpload">
-        <div class="control-group">
-            <label class="control-label">File: </label>
-            <div class="controls">
-                <input type="file" id="file" name="file" />
-            </div>
-        </div>
-        <div class="control-group">
-            <div class="controls">
-                <button class="btn btn-primary" id="submit" data-loading-text="uploading…" onclick="uploadStart()">Upload</button>
-            </div>
-        </div>
-    </form>
-</div>
+	<div>
+		<form name="form" method="post" action="generic_upload_server.php" enctype="multipart/form-data" target="iframeUpload">
+		<label class="control-label">File: </label>
+		<input type="file" id="file" name="file" />
+		<button id="submit" onclick="uploadStart()">Upload</button>
+		</form>
+	</div>
 </body>
 </html>
--- iframe_upload_server.php
+++ iframe_upload_server.php
@@ -1,7 +1,7 @@
 <?php
 if ($_FILES["file"]["error"] > 0) {
     echo "Error: " . $_FILES["file"]["error"] . "<br />";
-    echo "<script>parent.window.toggleModal(\"警告\", \"发生了未知错误\");parent.window.uploadOver();</script>";
+    echo "<script>parent.window.toggleModal(\"Connection Error\");parent.window.uploadOver();</script>";
 } else {
     echo "Upload: " . $_FILES["file"]["name"] . "<br />";
     echo "Type: " . $_FILES["file"]["type"] . "<br />";
@@ -11,15 +11,15 @@ if ($_FILES["file"]["error"] > 0) {
     $dir = "generic_upload/";
     if (file_exists($dir . $_FILES["file"]["name"])) {
         echo $_FILES["file"]["name"] . " already exists.";
-        echo "<script>parent.window.toggleModal(\"警告\", \"文件已存在\");parent.window.uploadOver();</script>";
+        echo "<script>parent.window.toggleModal(\"File already exists\");parent.window.uploadOver();</script>";
     } else {
         if (is_writeable($dir)) {
             move_uploaded_file($_FILES["file"]["tmp_name"], $dir . $_FILES["file"]["name"]);
             echo "Stored in: " . $dir . $_FILES["file"]["name"];
-            echo "<script>parent.window.toggleModal(\"提示\", \"上传成功\");parent.window.uploadOver();</script>";
+            echo "<script>parent.window.toggleModal(\"Upload successfully\");parent.window.uploadOver();</script>";
         } else {
             echo $dir ."is not writeable.";
-            echo "<script>parent.window.toggleModal(\"警告\", \"服务器离家出走了\");parent.window.uploadOver();</script>";
+            echo "<script>parent.window.toggleModal(\"Error occurred\");parent.window.uploadOver();</script>";
         }
     }
 }
